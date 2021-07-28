a70750ac26b05e3f926121d61c47e69b885cc3e4
hand <tkdwns0301@gmail.com>
2021-07-28 20:17:52 +0900

    [Add] intent JoinActivity
    2021_07_28

---
+++ Carrot/app/src/main/java/com/example/carrot/LogInActivity.kt
@@ -0,0 +1,21 @@
+package com.example.carrot
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LogInActivity : AppCompatActivity() {
+    val btnJoin: Button by lazy {
+        findViewById(R.id.btnJoin)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnJoin.setOnClickListener {
+            startActivity(Intent(this, JoinActivity::class.java))
+        }
+    }
+}
