14517a843596b11f14ebbfafbcb9ca520e6731ea
hand <tkdwns0301@gmail.com>
2021-07-29 19:16:03 +0900

    [Add] Password and TextColor
    2021_07_29

--- Carrot/app/src/main/java/com/example/carrot/LogInActivity.kt
+++ Carrot/app/src/main/java/com/example/carrot/LogInActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 
 class LogInActivity : AppCompatActivity() {
     val btnJoin: Button by lazy {
         findViewById(R.id.btnJoin)
     }
 
+    val btnStart: AppCompatButton by lazy {
+        findViewById(R.id.btnStart)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,10 @@ class LogInActivity : AppCompatActivity() {
 
         btnJoin.setOnClickListener {
             startActivity(Intent(this, JoinActivity::class.java))
+        }
+
+        btnStart.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
