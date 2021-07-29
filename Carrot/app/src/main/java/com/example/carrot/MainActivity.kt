fbb84ea075cf845cd211e3c4ffbdbeaf0e9c6f76
hand <tkdwns0301@gmail.com>
2021-07-29 20:46:57 +0900

    [Etc] none
    2021_07_29

--- Carrot/app/src/main/java/com/example/carrot/MainActivity.kt
+++ Carrot/app/src/main/java/com/example/carrot/MainActivity.kt
@@ -2,8 +2,22 @@ package com.example.carrot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem) : Boolean {
+        when(p0.itemId) {
+            R.id.homeItem -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragmentFrame, HomeFragment())
+                transaction.commit()
+                return true
+            }
+        }
+
+        return false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
