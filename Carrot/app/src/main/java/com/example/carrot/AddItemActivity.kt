5d8aee3f9f2ad18a5dd4d283462dc5446f8ce8be
hand <tkdwns0301@gmail.com>
2021-07-30 23:03:27 +0900

    [Add] back button Fun
    2021_07_30

--- Carrot/app/src/main/java/com/example/carrot/AddItemActivity.kt
+++ Carrot/app/src/main/java/com/example/carrot/AddItemActivity.kt
@@ -1,6 +1,8 @@
 package com.example.carrot
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_additem.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,5 +14,28 @@ class AddItemActivity : AppCompatActivity() {
 
         setSupportActionBar(addItemToolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_addtoolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            android.R.id.home -> {
+                //TODO 뒤로가기 버튼 눌렀을 때 이벤트 처리
+                finish()
+                return true
+            }
+            R.id.itemCompletion -> {
+                //TODO 완료버튼 눌렀을 때 이벤트 처리
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
