625441f4cc7012d94956ebee84b162839fd47a3d
hand <tkdwns0301@gmail.com>
2021-07-30 21:19:18 +0900

    [Add] Add Item button
    2021_07_30

--- Carrot/app/src/main/java/com/example/carrot/Fragment/HomeFragment.kt
+++ Carrot/app/src/main/java/com/example/carrot/Fragment/HomeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.carrot.Item.ListViewItem
 import com.example.carrot.R
 
 class HomeFragment : Fragment() {
@@ -12,5 +15,12 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val items = mutableListOf<ListViewItem>()
+
     }
 }
