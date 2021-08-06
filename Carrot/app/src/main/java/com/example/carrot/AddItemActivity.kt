9f15f4157899a236d1a833f53f95119078c76803
hand <tkdwns0301@gmail.com>
2021-08-06 14:03:53 +0900

    [Add] AddImageActivity
    2021_08_06

--- Carrot/app/src/main/java/com/example/carrot/AddItemActivity.kt
+++ Carrot/app/src/main/java/com/example/carrot/AddItemActivity.kt
@@ -1,13 +1,36 @@
 package com.example.carrot
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_additem.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class AddItemActivity : AppCompatActivity() {
+    private val btnAddImage: Button by lazy {
+        findViewById(R.id.btnAddImage)
+    }
+
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.img1))
+            add(findViewById(R.id.img2))
+            add(findViewById(R.id.img3))
+        }
+    }
+
+    private val imageUriList: MutableList<Uri> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_additem)
@@ -16,6 +39,8 @@ class AddItemActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        initBtnAddImage()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,7 +49,7 @@ class AddItemActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item?.itemId) {
+        when (item?.itemId) {
             android.R.id.home -> {
                 //TODO 뒤로가기 버튼 눌렀을 때 이벤트 처리
                 finish()
@@ -37,5 +62,95 @@ class AddItemActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun initBtnAddImage() {
+        btnAddImage.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    navigateImages()
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    requestPermissions(
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //TODO 권한이 부여된 것입니다.
+                    navigateImages()
+                } else {
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                //requestCode의 key값이 1000이 아닌 다른 값이 들어왔을 때
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun navigateImages() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) {
+            //정상적으로 이미지를 선택한 것이 아님
+            return
+        }
+        when (requestCode) {
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+
+                if (selectedImageUri != null) {
+                    //TODO 사진을 가져온 경우
+                    if (imageUriList.size > 3) {
+                        Toast.makeText(this, "이미 사진은 꽉 찼습니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+                    imageUriList.add(selectedImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
