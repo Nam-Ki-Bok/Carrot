87c47bb183614310b422d1df3a761a4396d02377
hand <tkdwns0301@gmail.com>
2021-08-10 19:40:18 +0900

    [Add] Activity_ItemSelected
    2021_08_10

---
+++ Carrot/app/src/main/java/com/example/carrot/ItemSelectedActivity.kt
@@ -0,0 +1,19 @@
+package com.example.carrot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_additem.*
+
+class ItemSelectedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_itemselected)
+
+        setSupportActionBar(addItemToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
