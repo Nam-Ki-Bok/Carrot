468737afa4603f4d628066ef5c3d3a1181a592c5
hand <tkdwns0301@gmail.com>
2021-08-12 00:35:06 +0900

    [Add] MainActivity
    2021_08_12

--- Carrot/app/src/main/java/com/example/carrot/View/MainActivity.kt
+++ Carrot/app/src/main/java/com/example/carrot/View/MainActivity.kt
@@ -1,73 +1,79 @@
 package com.example.carrot.View
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.carrot.Fragment.ChatFragment
 import com.example.carrot.Fragment.HomeFragment
 import com.example.carrot.Fragment.LifeFragment
 import com.example.carrot.Fragment.UserFragment
 import com.example.carrot.R
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_home.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
-    private val HomeFragment by lazy {
-        HomeFragment()
-    }
-    private val LifeFragment by lazy {
-        LifeFragment()
-    }
-    private val ChatFragment by lazy {
-        ChatFragment()
-    }
-    private val UserFragment by lazy {
-        UserFragment()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initNavigationBar()
-
-    }
-
-    private fun initNavigationBar() {
-        bottomNavigation.run {
-            setOnNavigationItemSelectedListener {
-                when (it.itemId) {
-                    R.id.homeItem -> {
-                        changeFragment(HomeFragment)
-                    }
-                    R.id.lifeItem -> {
-                        changeFragment(LifeFragment)
-                    }
-                    R.id.chatItem -> {
-                        changeFragment(ChatFragment)
-                    }
-                    R.id.userItem -> {
-                        changeFragment(UserFragment)
-                    }
-                }
-                true
+        if(savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<HomeFragment>(R.id.fragmentContainer)
             }
-            selectedItemId = R.id.homeItem
+        }
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeItem -> {
+                    moveHome()
+                    true
+                }
+                R.id.lifeItem -> {
+                    moveLife()
+                    true
+                }
+                R.id.chatItem -> {
+                    moveChat()
+                    true
+                }
+                R.id.userItem -> {
+                    moveUser()
+                    true
+                }
+                else -> false
+            }
         }
     }
 
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragmentContainer, fragment)
-            .commit()
+    private fun moveHome() {
+        supportFragmentManager.commit {
+            replace<HomeFragment>(R.id.fragmentContainer)
+        }
+    }
+    private fun moveLife() {
+        supportFragmentManager.commit {
+            replace<LifeFragment>(R.id.fragmentContainer)
+        }
+    }
+    private fun moveChat() {
+        supportFragmentManager.commit {
+            replace<ChatFragment>(R.id.fragmentContainer)
+        }
+    }
+    private fun moveUser() {
+        supportFragmentManager.commit {
+            replace<UserFragment>(R.id.fragmentContainer)
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_home_toolbar, menu)
-        return super.onCreateOptionsMenu(menu)
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.fragmentContainer, fragment)
+        }
     }
-
 }
