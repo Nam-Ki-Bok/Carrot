99c505cad8efa22f53de72e1f7719480599ef118
hand <tkdwns0301@gmail.com>
2021-08-13 19:43:46 +0900

    [Add] HomeFragment
    2021_08_13

--- Carrot/app/src/main/java/com/example/carrot/Fragment/HomeFragment.kt
+++ Carrot/app/src/main/java/com/example/carrot/Fragment/HomeFragment.kt
@@ -2,53 +2,43 @@ package com.example.carrot.Fragment
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.carrot.Adapter.ListAdapter
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.carrot.R
+import com.example.carrot.View.AddItemActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
-class HomeFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater!!.inflate(R.layout.fragment_home, container, false)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
-        return view
+    private val writeContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-
-
-        //TODO item에 데이터 추가 시 사용하는 코드
-        /*
-        val items = mutableListOf<ListViewItem>()
-        items.add(ListViewItem(activity?.let {
-            ContextCompat.getDrawable(
-                it,
-                R.drawable.ic_baseline_image_search_24
-            )
-        }!!, "제목", "20000"))
-        */
-
-        //TODO item 클릭 시 이벤트 처리
-        /*lvHome.setOnItemClickListener { parent: AdapterView<*>, view: View, position: Int, id: Long ->
-            val item = parent.getItemAtPosition(position) as ListViewItem
-            Toast.makeText(activity, item.title, Toast.LENGTH_SHORT).show()
-        }*/
-
+        init(view)
     }
 
+    private fun init(view: View) {
+        initWriteButton(view)
+    }
 
-    private fun addItemList() {
+    private fun initWriteButton(view: View) {
+        val btnWriteSale = view.findViewById<FloatingActionButton>(R.id.btnWriteSale)
 
+        btnWriteSale.setOnClickListener {
+            moveWriteSale()
+        }
+    }
+
+    private fun moveWriteSale() {
+        val intent = Intent(activity, AddItemActivity::class.java)
+        startActivity(intent)
     }
 
 }
