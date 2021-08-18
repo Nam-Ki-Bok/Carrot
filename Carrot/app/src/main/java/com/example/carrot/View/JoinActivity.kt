c5a76b7f9bcf6171ae3fd4436c4cfa3a3ce4d6bf
hand <tkdwns0301@gmail.com>
2021-08-18 15:53:13 +0900

    [Update] LoginLayout
    2021_08_18

--- Carrot/app/src/main/java/com/example/carrot/View/JoinActivity.kt
+++ Carrot/app/src/main/java/com/example/carrot/View/JoinActivity.kt
@@ -4,19 +4,72 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import com.example.carrot.Network.RetrofitClient
 import com.example.carrot.R
+import com.example.carrot.Service.AuthService
+import kotlinx.android.synthetic.main.activity_join.*
+import retrofit2.Retrofit
 
-class JoinActivity: AppCompatActivity() {
-    val btnLogIn: AppCompatButton by lazy {
-        findViewById(R.id.btnLogIn)
-    }
+class JoinActivity : AppCompatActivity() {
+    private lateinit var retrofit: Retrofit
+    private lateinit var joinService: AuthService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
 
-        btnLogIn.setOnClickListener {
-            //TODO 입력된 정보를 데이터베이스에 넣고, 다시 로그인 화면으로 돌아간다.
-            startActivity(Intent(this, LogInActivity::class.java))
+        init()
+    }
+
+    private fun init() {
+        initView()
+        initRetrofit()
+    }
+
+    private fun initView() {
+        initName()
+        initPhoneNum()
+        initPassword()
+        initNext()
+    }
+
+    private fun initRetrofit() {
+        retrofit = RetrofitClient.getInstance()
+        joinService = retrofit.create(AuthService::class.java)
+    }
+
+    private fun initName() {
+        etName.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                if (etName.text.isNullOrEmpty()) {
+                    etName.error = "이름을 입력해주세요."
+                } else {
+                    etName.error = null
+                }
+
+            }
+        }
+    }
+
+    private fun initPhoneNum() {
+        etPhoneNum.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                validatePhoneNum()
+            }
+        }
+    }
+
+    private fun validatePhoneNum() : Boolean {
+        return if (etPhoneNum.text.isNullOrEmpty()) {
+            etPhoneNum.error = "휴대폰 번호를 입력해주세요."
+            false
+        } else {
+            val phoneNum = etPhoneNum.text.toString()
+            val isValid = android.util.Patterns.PHONE.matcher(phoneNum).matches()
+
+            if(!isValid) {
+
+            }
         }
     }
 }
