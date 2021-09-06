75b5263bce2e1b41cb4fef4770e38bf0da7556eb
hand <tkdwns0301@gmail.com>
2021-09-06 17:10:21 +0900

    [Add] Detail Sale Layout and Activity
    2021_09_06

---
+++ Carrot/app/src/main/java/com/example/carrot/View/DetailSaleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.carrot.View
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.carrot.R
+
+class DetailSaleActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_sale)
+    }
+}
