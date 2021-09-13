12b1e0f1c6f61704ec4d502cc64db76adbb735a7
Nam-Ki-Bok <nkb7714@naver.com>
2021-09-13 14:22:04 +0900

    [2021.09.013]

--- Carrot/app/src/main/java/com/example/carrot/Network/RetrofitClient.kt
+++ Carrot/app/src/main/java/com/example/carrot/Network/RetrofitClient.kt
@@ -2,21 +2,34 @@ package com.example.carrot.Network
 
 import com.example.carrot.R
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private var instance: Retrofit? = null
     private val gson = GsonBuilder().setLenient().create()
 
-//    private const val BASE_URL = "http://ec2-3-34-172-246.ap-northeast-2.compute.amazonaws.com:8080/"
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "http://ec2-3-34-172-246.ap-northeast-2.compute.amazonaws.com:8080/"
+    private const val CONNECT_TIMEOUT_SEC = 20000L
 
     fun getInstance(): Retrofit {
         if(instance == null) {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .connectTimeout(CONNECT_TIMEOUT_SEC, TimeUnit.SECONDS)
+                .build()
+
             instance = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
                 .build()
         }
 
